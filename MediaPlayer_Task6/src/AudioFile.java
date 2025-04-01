2655884ba55b0d84039d3bb26cc401f376081fc6
Luis Stanglmeier <luisl0101@yahoo.de>
2025-04-01 11:52:49 +0200

    first commit

---
+++ MediaPlayer_Task6/src/AudioFile.java
@@ -0,0 +1,22 @@
+public class AudioFile {
+    public static void Main()
+    {
+
+    }
+
+    public void AudioFile()
+    {
+
+    }
+
+    public void parsePathname(String path)
+    {
+
+    }
+
+    public String getPathname(String Path)
+    {
+        String pathName = "";
+        return pathName;
+    }
+}
